8a1f62a7196a6c68c2314d20563d516cf0e8de56
greencarrot5 <seppevanammel@gmail.com>
2024-11-05 14:45:19 +0100

    :sparkles: Added Tilemap.js

---
+++ Tilemap.js
@@ -0,0 +1,82 @@
+class Tilemap {
+
+	constructor(tilesize, textures={}) {
+
+		this.tilesize = tilesize;
+		this.textures = textures;
+
+		this.tiles = [];
+
+	}
+
+	setTile(x, y, tile) {
+
+		if (this.tiles.length == 1) {
+		
+			if (this.tiles[0][0] < x || (this.tiles[0][0] == x && this.tiles[0][1] < y)) {
+
+				this.tiles.push([x, y, tile]);
+				
+			} else {
+
+				this.tiles.shift([x, y, tile]);
+
+			}
+
+		}
+		
+		//Binary search go brrrr
+
+		let minI = 0;
+		let maxI = this.tiles.length;
+
+		while (maxI - minI > 1) {
+			
+			let searchI = Math.ceil((minI + maxI) / 2);
+
+			if (this.tiles[searchI][0] == x && this.tiles[searchI][1] == y) {
+
+				//Special case: this tile needs to be overwritten
+				minI = searchI;
+				maxI = searchI;
+				this.tiles.splice(searchI, 1);
+
+			} else if (this.tiles[searchI][0] < x || (this.tiles[searchI][0] == x && this.tiles[searchI][1] < y)) {
+				
+				minI = searchI;
+
+			} else {
+
+				maxI = searchI;
+
+			}
+
+		}
+
+		this.tiles.splice(maxI, 0, [x, y, tile]);
+
+	}
+
+	getTile(x, y) {
+
+	}
+
+	render(canvas, x, y) {
+		
+		let context = canvas.getContext("2d");
+
+		for (let tile of this.tiles) {
+			
+			let tileX = tile[0];
+			let tileY = tile[1];
+			let texture = this.textures[tile[2]];
+			
+			context.drawImage(texture, x + tileX*this.tilesize, y + tileY*this.tilesize, this.tilesize, this.tilesize);
+
+		}
+
+	}
+
+}
+
+export { Tilemap };
